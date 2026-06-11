25df84ffb1369cff4ab87824cae7cc6706b1d890
aseemcug25me-prog <aseemc.ug25.me@nitp.ac.in>
2026-06-11 08:33:34 +0530

    Success

---
+++ Getters_Setters.py
@@ -0,0 +1,14 @@
+class MyClass:
+    def __init__(self, value):
+        self._value = value
+    
+    def show(self):
+        print(f"Value is {self._value}")
+
+    
+    @property
+    def ten_value(self):
+        return 10 * self._value
+
+obj = MyClass(10)
+obj.show()
