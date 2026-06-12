3dd6893fee214d1205536c3169b57ee89a7e0798
aseemcug25me-prog <aseemc.ug25.me@nitp.ac.in>
2026-06-12 16:48:32 +0530

    Instance Variable

---
+++ Instance_and_Class_Variable.py
@@ -0,0 +1,13 @@
+class Employee:
+    companyName = "Apple"
+    def __init__(self, name):
+        self.name = name
+        self.raise_amount = 0.02
+    def showDetails(self):
+        print(f"The name of the Employee is {self.name} and his raise amount in {self.companyName} is {self.raise_amount}")
+
+emp1 = Employee("Harry")
+emp1.raise_amount = 0.3
+emp1.showDetails()
+emp2 = Employee("Aseem")
+emp2.showDetails()
